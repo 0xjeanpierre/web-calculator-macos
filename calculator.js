0845cf07ebb8cc47c11b9c27948f862c3a1d5bab
Jean Pierre <jeanpierreoliveira@live.com>
2022-09-04 23:09:28 -0300

    first commit

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function showResults() {
+    var resText = document.getElementById('results');
+    resText.setAttribute('value','1');
+}
+function cancelEntry() {
+    var resText = document.getElementById('results');
+    resText.setAttribute('value','0');
+}
+function addEntry(btn) {
+    var res = document.getElementById('results');
+    resValue = res.value;
+    newValue = btn.value ;
+    if (resValue == "0"){
+        resValue = "";
+    }
+    value = resValue + newValue;
+    res.setAttribute('value', value);
+}
